fe6278acdbdd55194ed6787450bd467696894d22
Haroon203 <haroon00@yahoo.com>
2025-03-10 01:12:47 +0500

    Generate Matrix and Initialize with Random Numbers

--- Task1_matrix_transpose.c
+++ Task1_matrix_transpose.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20 //  define Matrix size 20x20
+
+void generateMatrix(int matrix[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i][j] = rand() % 100; // Random values store in Matrix NxN
+        }
+    }
+}
+
 int main()
 {
-   
+    srand(time(NULL));
+    int matrix[N][N];
+    generateMatrix(matrix);
+
     return 0;
 }
