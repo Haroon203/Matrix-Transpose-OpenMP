f3be4d3416492c709980ff0df8e0ddecc7f3090c
Haroon203 <haroon00@yahoo.com>
2025-03-10 01:14:09 +0500

    Transpose of Matrix

--- Task1_matrix_transpose.c
+++ Task1_matrix_transpose.c
@@ -13,12 +13,22 @@ void generateMatrix(int matrix[N][N])
         }
     }
 }
+void transposeSequential(int matrix[N][N], int transposed[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            transposed[j][i] = matrix[i][j]; // transposed of matrix row column swap
+        }
+    }
+}
 
 int main()
 {
     srand(time(NULL));
     int matrix[N][N];
     generateMatrix(matrix);
-
+    int transposed[N][N];
     return 0;
 }
